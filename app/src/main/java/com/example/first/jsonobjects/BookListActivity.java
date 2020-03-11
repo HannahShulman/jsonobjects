9921586bcba9cfff00686ce5332e650973e1f10c
Ayal <ayalfishey@gmail.com>
2020-03-11 17:12:58 +0200

    BookList for Array. does not work yet needs more googling

--- app/src/main/java/com/example/first/jsonobjects/BookListActivity.java
+++ app/src/main/java/com/example/first/jsonobjects/BookListActivity.java
@@ -3,6 +3,20 @@ package com.example.first.jsonobjects;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class BookListActivity extends AppCompatActivity {
 
@@ -10,6 +24,16 @@ public class BookListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+        String jsonString = getIntent().getStringExtra("books");
+        JsonArray jsonArray = JsonParser.parseString(jsonString).getAsJsonArray();
+        ArrayList<Book> bookArrayList;
+        Type bookListType = new TypeToken<ArrayList<Book>>(){}.getType();
+        bookArrayList = new Gson().fromJson(jsonArray, bookListType);
+        for (Book book: bookArrayList) {
+            TextView tv = findViewById(R.id.textView);
+            tv.setText(book.name);
+
+        }
 
     }
 }
