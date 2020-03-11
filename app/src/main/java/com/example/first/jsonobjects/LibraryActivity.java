0b03c2e65cc26f73e664f17c1abe85efdeba967b
MarianoPtascinsky <mariano.ptascinsky@gmail.com>
2020-03-11 08:06:06 +0200

    I cannot find my error. It continues crashing

--- app/src/main/java/com/example/first/jsonobjects/LibraryActivity.java
+++ app/src/main/java/com/example/first/jsonobjects/LibraryActivity.java
@@ -3,6 +3,10 @@ package com.example.first.jsonobjects;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 class LibraryActivity extends AppCompatActivity {
 
@@ -10,6 +14,18 @@ class LibraryActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.library_layout);
+
+            RecyclerView bookList = findViewById(R.id.library);
+
+            bookList.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+            ArrayList<Book> list = new ArrayList<>();
+            final LibraryAdapter adapter = new LibraryAdapter(list);
+
+            //attach adapter to recyclerview
+            bookList.setAdapter(adapter);
+
+
 
 
     }
